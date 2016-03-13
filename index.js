e015286aa50dc1fe28177ebab690acca89bcf742
Toshiaki Yoshida <yositosi@gmail.com>
2016-03-14 03:33:20 +0900

    fix https://github.com/Draivin/node-google-spreadsheet/commit/2a6aff7afec143a273d817da9f6170501e25e68a

--- index.js
+++ index.js
@@ -116,9 +116,9 @@ var GooogleSpreadsheet = function( ss_key, auth_id, options ){
         if ( method == 'POST' || method == 'PUT' ){
           headers['content-type'] = 'application/atom+xml';
         }
-        if ( method == 'PUT' ){
+        if (method == 'PUT' || method == 'POST' && query_or_data.includes('http://schemas.google.com/gdata/batch') ) {
           headers['If-Match'] = '*';
-        } 
+        }
 
         if ( method == 'GET' && query_or_data ) {
           url += "?" + querystring.stringify( query_or_data );
@@ -473,6 +473,3 @@ var xmlSafeColumnName = function(val){
   return String(val).replace(/[\s_]+/g, '')
       .toLowerCase();
 }
-
-
-
